71420a1b7ea2d2e54e23f5fb6e42d7ce5a4c516a
Jeff Cohen <jeff@codeacademy.org>
2013-02-11 14:33:52 -0600

    Nicer redirects and voting index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,8 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
     @movie = Movie.find(params[:id])
+    @vote = Vote.new
+    @vote.movie_id = @movie.id
 
     respond_to do |format|
       format.html # show.html.erb
