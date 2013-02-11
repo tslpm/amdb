fb5556cfb7d151e0edf7148c5e3301b67b6b7269
Jeff Cohen <jeff@codeacademy.org>
2013-02-11 14:11:43 -0600

    Try to show vote page inside the movie page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote < ActiveRecord::Base
   attr_accessible :movie_id, :user_id
 
+  validates_presence_of :movie_id
+  validates_presence_of :user_id
+
   belongs_to :movie
 
   belongs_to :user
